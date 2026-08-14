9df0803004ee5ca1c2d46ef8d5b3f4ffbf367b9b
dev-tarun <tarun.yadav42009@gmail.com>
2026-08-14 18:23:51 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,4 @@
+user="tarun"
+age= 17
+print("Welcome : ",user)
+print("Your Age is: ",age)
---
+++ 2.py
@@ -0,0 +1,25 @@
+no1=int(input("Enter the 1st number:"))
+no2=int(input("Enter the 2nd number:"))
+
+
+no1+=no2
+print("Addition is ",no1)
+
+no1-=no2
+print("Subtraction is",no1)
+
+no1*=no2
+print("Multiplication is",no1)
+
+no1/=no2
+print("Division is",no1)
+
+no1//=no2
+print("Floor Division is",no1)
+
+no1**=no2
+print("Exponent is",no1)
+
+no1%=no2
+print("Remainder is",no1)
+
