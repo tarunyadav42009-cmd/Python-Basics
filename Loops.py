8277e6f00555ebde8a02c7585b4e7ef4727a36d2
dev-tarun <tarun.yadav42009@gmail.com>
2026-08-17 19:05:43 +0530

    :wq

---
+++ Loops.py
@@ -0,0 +1,27 @@
+'''
+
+i=1
+while i<=5:
+    print(i)
+    i+=1
+
+'''
+
+'''
+for i in range(5):
+
+    print(i)
+
+'''
+
+'''
+
+for i in range(1,5+1,1):
+    print(i,end="-")
+
+'''
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print(" ")
