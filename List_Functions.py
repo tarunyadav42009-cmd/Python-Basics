761e4eec3aefda2cfd2e2ca54c2455459ca81aa9
dev-tarun <tarun.yadav42009@gmail.com>
2026-08-19 19:02:46 +0530

    :wq

---
+++ List_Functions.py
@@ -0,0 +1,14 @@
+# Replacing values
+num=[90,12,81,72,54,45]
+
+print("Before replacing values:",num)
+
+num[2]=171
+
+print("After replacing values;",num)
+
+#Deleting Values 
+
+del num[4]
+
+print("After deleting value:",num)
