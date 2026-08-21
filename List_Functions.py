f5f4552f8f3d9aec67cc1a41535cfd434b1715ea
dev-tarun <tarun.yadav42009@gmail.com>
2026-08-21 18:21:04 +0530

    :wq

--- List_Functions.py
+++ List_Functions.py
@@ -5,16 +5,19 @@ print("Before replacing values:",num)
 
 num[2]=171
 
-print("After replacing values;",num)
+print("After replacing values:",num)
 
 #Deleting Values 
-
 del num[4]
 
 print("After deleting value:",num)
 
 #Appending List
-
 num.append(90)
 
 print("After Appending Values:",num)
+
+#Pop function in list
+num.pop(4)
+
+print("After Pop function:",num)
