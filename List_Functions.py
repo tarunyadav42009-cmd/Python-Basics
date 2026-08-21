fa50314381809a3b73056b31c4145165abc4e9f0
dev-tarun <tarun.yadav42009@gmail.com>
2026-08-21 19:21:30 +0530

    :wq

--- List_Functions.py
+++ List_Functions.py
@@ -21,3 +21,51 @@ print("After Appending Values:",num)
 num.pop(4)
 
 print("After Pop function:",num)
+
+#Copy Function
+val=[" "]
+
+val=num.copy()
+
+print("Copy:",val)
+
+
+#pop
+
+a=num.pop(1)
+
+print("pop value:",a)
+
+a1=num.pop()
+
+print(num)
+
+num.reverse()
+
+
+print(num)
+
+num1=num.copy()
+
+print(num1)
+
+num1.append(101)
+
+print(num1)
+
+'''
+list 
+to print all the records 
+to print record in range wise 
+replace the value 
+delete  the value 
+find the length of list 
+find max value from list , min
+add record in list index wise 
+add record to the last 
+count the total value in list
+find the index value of particular record
+give the pop example
+reverse the list 
+copy from one list to another
+'''
---
+++ list_prac.py
@@ -0,0 +1,26 @@
+'''
+list 
+to print all the records 
+to print record in range wise 
+replace the value 
+delete  the value 
+find the length of list 
+find max value from list , min
+add record in list index wise 
+add record to the last 
+count the total value in list
+find the index value of particular record
+give the pop example
+reverse the list 
+copy from one list to ano
+'''
+TSG=[31,13,445,657,78]
+print(TSG)
+TSG.insert(3,34)
+print(TSG)
+
+TSG[0]=45
+print(TSG)
+
+del TSG [3]
+print(TSG)
