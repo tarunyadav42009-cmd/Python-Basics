953d65c0d7a82b67533994f106ce5c646e92877e
dev-tarun <tarun.yadav42009@gmail.com>
2026-08-31 19:08:12 +0530

    :wq

---
+++ arr.py
@@ -0,0 +1,17 @@
+
+'''
+l1=input("Enter name").split()
+
+print(l1)
+
+print(l1[1])
+'''
+
+arr1=[]
+
+n=int(input("Enter Number of Elements: "))
+
+for i in range(0,n):
+    e1=input("Enter name: ")
+    arr1.append(e1)
+print(arr1)
---
+++ tuple.py
@@ -0,0 +1,16 @@
+tup1=(12,91,12,'abc','pqr');
+
+print(tup1[0])
+
+print(tup1)
+
+print(tup1[1:4])
+
+tup2=(45,90,72,117)
+
+tup3=tup1+tup2
+
+print(tup3)
+
+print(len(tup1))
+
