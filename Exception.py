55df7a4a98da615da402c365a5ba37e899de2771
dev-tarun <tarun.yadav42009@gmail.com>
2026-09-02 18:49:24 +0530

    :wq

---
+++ Exception.py
@@ -0,0 +1,49 @@
+'''
+a=int(input("Enter first number: "))
+b=int(input("Enter Second Number: "))
+
+
+try:
+    c=a/b
+    print(c)
+
+except ZeroDivisionError:
+    print("Division by zero")
+
+'''
+
+'''
+x=23
+
+if x<0:
+    raise Exception("Sorry! no negative numbers allowed!")
+
+'''
+'''
+list=[1,2,3] 
+
+try:
+    a=list[1]
+    print(a)
+except  IndexError:
+    print("Out of range")
+'''
+    
+'''
+a=int("abc")
+ṣ
+print(a)
+'''
+
+'''
+try:
+    a=int("abc")
+except ValueError as e:
+    print(e)
+
+'''
+
+try:
+    a=int("abc")
+except ValueError:
+    print("Value Error!!!")
